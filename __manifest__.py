da8a041687c181cf8205eaee0fc2be2bd01b8f26
arash <arash@Kimya.kimyagroup.org>
2023-12-27 01:01:04 -0500

    init 1.0.0

--- __manifest__.py
+++ __manifest__.py
@@ -16,7 +16,7 @@
     # for the full list
     'category': 'Service Desk/Service Desk',
     'application': True,
-    'version': '1.0.3',
+    'version': '1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'web', 'mail', 'project', 'sd_apps', 'hr'],
